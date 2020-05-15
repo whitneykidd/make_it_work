dedae46a5c3cf543e8dc912e3096c23f96e0fe64
Whitney Kidd <whitneymkidd@gmail.com>
2020-05-15 09:05:17 -0600

    pass relationships tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project <ApplicationRecord
   validates_presence_of :name, :material
   belongs_to :challenge
+  has_many :contestant_projects
+   has_many :contestants, through: :contestant_projects
 end
